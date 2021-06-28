57a142ba36b70afbfd164d9c2576f8af52837318
Oliver Borg <BRGOLI005@myuct.ac.za>
2021-06-28 13:13:14 +0200

    Changed storage format and implemented autoincrement

--- server.py
+++ server.py
@@ -1,11 +1,17 @@
 from flask import Flask, request, render_template, flash, make_response, redirect, url_for
 import datetime
 import dateutil.parser
-from werkzeug.datastructures import Range
+import time
+import atexit
 
+from werkzeug.utils import secure_filename
+
+from apscheduler.schedulers.background import BackgroundScheduler
 
 app = Flask(__name__)
 app.secret_key = "dev"
+
+
 
 @app.route('/')
 def index():
@@ -14,7 +20,7 @@ def index():
 
 @app.route('/index', methods=['GET', 'POST'])
 def form():
-    labels = counterList()
+    labels = getList("cookies/labels.txt")
     if request.method =="POST":
         if request.form.get("quotes"):
             return quotes()
@@ -28,13 +34,13 @@ def form():
             name = name.replace("\n","")
             dupe = False
             for l in labels:
-                if name in l:
+                if name in l[0]:
                     dupe = True
             if not dupe:
-                with open("cookies/labels.txt","a") as labels_f:
-                    if increment:
-                        name = "auto"+name
-                    labels_f.write("\n#"+name+"\n#0")
+                new_counter = [name,increment,0]
+                labels.append(new_counter)
+                with open("cookies/labels.txt","w") as labels_f:
+                    labels_f.write(str(labels))
                 
 
 
@@ -47,7 +53,7 @@ def form():
     
     print(labels)
     #autoIncrement()
-    labels = counterList()
+    labels = getList("cookies/labels.txt")
     return render_template("index.html", labels=labels)
 
 @app.route('/outofcontext', methods=['GET', 'POST'])
@@ -56,23 +62,21 @@ def quotes():
         if request.form.get("postquote"):
             new_quote = request.form["newquote"]
             user = request.form["homies"]
-            with open("cookies/quotes.txt", "a") as quotes_f:
-                if new_quote.strip() !="":
-                    quotes_f.write("\n#" + new_quote + "-" + user +" "+ datetime.datetime.now().strftime("%d/%m/%Y"))
+            new_item = [new_quote.strip(),user,datetime.datetime.now().strftime("%d/%m/%Y")]
+            quotes = getList("cookies/quotes.txt")
+            if new_quote.strip() !="":
+                quotes.append(new_item)
+            with open("cookies/quotes.txt", "w") as quotes_f:
+                    quotes_f.write(str(quotes))
         elif request.form.get("counters"):
             return form()
         else:
-            quotes = quoteList()
-            for quote in quotes:
-                query = (quote[0]+quote[1])
-                #print(query)
-                #print(request.form[query])
-                if request.form.get(query):
-                    #print("Found")
-                    quotes.remove(quote)
+            quotes = getList("cookies/quotes.txt")
+            for i in range(len(quotes)):
+                if request.form.get(str(i+1)):
+                    quotes.remove(quotes[i])
                     with open("cookies/quotes.txt", "w") as quotes_f:
-                        for line in quotes:
-                            quotes_f.write("\n#" + line[0] +line[1].strip())
+                        quotes_f.write(str(quotes))
                     break
 
 
@@ -80,61 +84,31 @@ def quotes():
     
              
         #print(quotes)
-    quotes = quoteList()            
+    quotes = getList("cookies/quotes.txt")            
     return render_template("outofcontext.html", quotes=quotes)
 
 
-def quoteList():
-    quotes = []
-    with open("cookies/quotes.txt", "r") as quotes_f:
+def getList(file):
+    with open(file, "r") as quotes_f:
         lines = quotes_f.readlines()
         line = ""
         for l in lines:
             line+=l
-        lines = line.split("#")
-        #print(lines)
-        for l in lines:
-            temp = []
-            temp.append(l[:l.find("-")])
-            temp.append(l[l.find("-"):])
-            if temp[0].strip() !="":
-                quotes.append(temp)
-    return quotes
+        if line.strip()=="":
+            line="[]"
+        return eval(line)
 
-def counterList():
-    counters = []
-    with open("cookies/labels.txt", "r") as counters_f:
-        lines = counters_f.readlines()
-        line = ""
-        for l in lines:
-            line+=l
-        lines = line.split("#")[1:]
-        print(lines)
-        for i in range(0,len(lines),2):
-            l = lines[i].strip()
-            count = lines[i+1].strip()
-            if l.strip() !="":
-                if l.find("auto") ==0:
-                    l = l[4:]
-                counters.append(l + ": " + count)
-    return counters
 def autoIncrement():
-    counters = []
-    with open("cookies/labels.txt", "r") as counters_f:
-        lines = counters_f.readlines()
-        line = ""
-        for l in lines:
-            line+=l
-        lines = line.split("#")[1:]
-        print(lines)
-        for i in range(0,len(lines),2):
-            l = lines[i].strip()
-            count = lines[i+1].strip()
-            if l.strip() !="":
-                if l.find("auto") ==0:
-                    count = str(int(count) + 1)
-                counters.append(l)
-                counters.append(count)
+    counters = getList("cookies/labels.txt")
+    for counter in counters:
+        if counter[1] == True:
+            counter[2]+=1
     with open("cookies/labels.txt", "w") as counters_f:
-        for l in counters:
-            counters_f.write("#"+l+"\n")
+        counters_f.write(str(counters))
+    print(datetime.datetime.now())
+
+scheduler = BackgroundScheduler()
+date = datetime.datetime.now()
+scheduler.add_job(func=autoIncrement,trigger="cron",hour=0,minute=0,second=1)
+scheduler.start()
+atexit.register(lambda:scheduler.shutdown())
