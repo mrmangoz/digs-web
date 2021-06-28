fdb48d4af508239c342c089b8a49660c231cf683
Ciaran Manca <ciaranmanca@gmail.com>
2021-06-28 16:54:34 +0200

    implemented mongoDB

--- server.py
+++ server.py
@@ -1,8 +1,8 @@
 from flask import Flask, request, render_template, flash, make_response, redirect, url_for
 import datetime
-import dateutil.parser
 import time
 import atexit
+import pymongo
 
 from werkzeug.utils import secure_filename
 
@@ -10,7 +10,8 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 app = Flask(__name__)
 app.secret_key = "dev"
-
+client = pymongo.MongoClient()
+db = client.digs
 
 
 @app.route('/')
@@ -20,89 +21,83 @@ def index():
 
 @app.route('/index', methods=['GET', 'POST'])
 def form():
-    labels = getList("cookies/labels.txt")
+    labels = getLabels()
     if request.method =="POST":
         if request.form.get("quotes"):
-            return quotes()
+            return redirect('/outofcontext')
         elif request.form.get("addtally"):
-            try: 
+            try:
                 request.form["autoincrement"]
                 increment = True
             except:
                 increment = False
             name = request.form["new_tally_name"]
-            name = name.replace("\n","")
+            name = name.strip("\n")
             dupe = False
             for l in labels:
                 if name in l[0]:
                     dupe = True
-            if not dupe and name.strip()!="":
-                new_counter = [name,increment,0]
-                labels.append(new_counter)
+            if not dupe and name != "":
+                db.tallies.insert_one({"label":name, "status": increment, "count": 0})
         else:
             for i in range(len(labels)):
                 if request.form.get(str(i+1)+"d"):
-                    labels.remove(labels[i])
+                    db.tallies.find_one_and_delete({"label": labels[i][0]})
                     break
                 elif request.form.get(str(i+1)+"+"):
-                    labels[i][2]+=1
+                    db.tallies.find_one_and_update({"label": labels[i][0]},
+                                                   {"$inc" : {"count": 1}})
                     break
                 elif request.form.get(str(i+1)+"-"):
-                    labels[i][2]-=1
+                    db.tallies.find_one_and_update({"label": labels[i][0] },
+                                                   {"$inc" : {"count": -1}})
                     break
                 elif request.form.get(str(i+1)+"r"):
-                    labels[i][2]=0
+                    db.tallies.find_one_and_update({"label": labels[i][0]},
+                                                   {"$set" : {"count": 0}})
                     break
-        with open("cookies/labels.txt","w") as labels_f:
-            labels = sortList(labels)
-            labels_f.write(str(labels))
-    
-    #print(labels)
-    labels = getList("cookies/labels.txt")
+        # with open("cookies/labels.txt","w") as labels_f:
+        #     labels = sortList(labels)
+        #     labels_f.write(str(labels))
+
+    labels = getLabels()
     return render_template("index.html", labels=labels)
 
 @app.route('/outofcontext', methods=['GET', 'POST'])
 def quotes():
     if request.method =="POST":
         if request.form.get("postquote"):
-            new_quote = request.form["newquote"]
-            user = request.form["homies"]
-            new_item = [new_quote.strip(),user,datetime.datetime.now().strftime("%d/%m/%Y")]
-            quotes = getList("cookies/quotes.txt")
-            if new_quote.strip() !="":
-                quotes.append(new_item)
-            with open("cookies/quotes.txt", "w") as quotes_f:
-                    quotes_f.write(str(quotes))
+            new_quote = str(request.form["newquote"].strip())
+            user = str(request.form["homies"])
+            date = str(datetime.datetime.now().strftime("%d/%m/%Y"))
+            if new_quote != "":
+                db.quotes.insert_one({"quote":new_quote,"author":user,"date":date})
         elif request.form.get("counters"):
-            return form()
+            return redirect('/index')
         else:
-            quotes = getList("cookies/quotes.txt")
+            quotes = getQuotes()
             for i in range(len(quotes)):
                 if request.form.get(str(i+1)):
-                    quotes.remove(quotes[i])
-                    with open("cookies/quotes.txt", "w") as quotes_f:
-                        quotes_f.write(str(quotes))
+                    db.quotes.find_one_and_delete({"quote": quotes[i][0]})
                     break
+    quotes = getQuotes()
 
-
-
-    
-             
-        #print(quotes)
-    quotes = getList("cookies/quotes.txt")            
     return render_template("outofcontext.html", quotes=quotes)
 
 
-def getList(file):
-    with open(file, "r") as quotes_f:
-        lines = quotes_f.readlines()
-        line = ""
-        for l in lines:
-            line+=l
-        if line.strip()=="":
-            line="[]"
-        return eval(line)
-        
+def getQuotes():
+    quotes = []
+    for quote in db.quotes.find():
+        quotes += [[quote["quote"], quote["author"], quote["date"]]]
+    quotes.reverse()
+    return quotes
+
+def getLabels():
+    labels = []
+    for label in db.tallies.find():
+        labels += [[label["label"], label["status"], label["count"]]]
+    return sortList(labels)
+
 def sortList(labels):
     for i in range(len(labels)):
         maxi = i
@@ -119,13 +114,10 @@ def sortList(labels):
 
 
 def autoIncrement():
-    counters = getList("cookies/labels.txt")
-    for counter in counters:
-        if counter[1] == True:
-            counter[2]+=1
-    with open("cookies/labels.txt", "w") as counters_f:
-        counters_f.write(str(counters))
-    print(datetime.datetime.now())
+    for counter in db.tallies.find():
+        if counter["status"]:
+            db.tallies.find_one_and_update({"label": counter["label"]},
+                                           {"$inc" : {"count": 1}})
 
 scheduler = BackgroundScheduler()
 date = datetime.datetime.now()
